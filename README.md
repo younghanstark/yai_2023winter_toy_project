20eab8038075890b43b0f4a0bed11af05d55e19f
younghanstark <younghanpark@yonsei.ac.kr>
2023-03-05 20:03:39 +0900

    data update, readme update

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # yai_2023winter_toy_project
 
-Dataset generator !
+Youtube thumbnail dataset generator ... by younghanstark
+
+## Requirements
+```bash
+pip install google-api-python-client
+```
+
+## File Explanation
+- `/thumbnails`: (Ignored) Dataset folder.
+- `.gitignore`
+- `balanceData.py`: A code can be used to balance dataset.
+- `categories.py`: A code retrieves available video categories in US.
+- `channels.pickle`: A pickle file that saves type 'set' contains channels already processed.
+- `getChannels.py`: A code that retrieves random channel ids.
+- `key.txt`: (Ignored) A text file that should contain your API key.
+- `makeData.py`: A code that makes dataset from a list of channel ids.
+- `metadata.csv`: Metadata of dataset.
+- `README.md`: Me myself! :)
+
+## Usage
+1. Get your API key(Youtube Data API v3) on Google Cloud Platform
+2. Run `getChannels.py` to get a list of random channel ids.
+3. Run `makeData.py` to make dataset.
+4. Carefully inspect your dataset and balance your dataset using `balanceData.py`.
+
+## Note
+- Always think of your quota limit. Especially `balanceData.py` uses large amount of quota points.
--- balanceData.py
+++ balanceData.py
@@ -3,7 +3,8 @@
 
 import os
 import csv
-# from googleapiclient.discovery import build
+from googleapiclient.discovery import build
+import urllib.request
 
 PATH = './thumbnails/images'
 
@@ -13,9 +14,64 @@ for root, dirs, files in os.walk(PATH):
         categories[dir] = len(os.listdir(os.path.join(root, dir)))
 print(categories)
 
-maxNumber = categories[max(categories, key=categories.get)]
-for key, value in categories.items():
-    categories[key] = maxNumber - value
-    print(f'For {key}, need {categories[key]} more data.')
+with open('key.txt', 'r') as f:
+    apiKey = f.read()
 
-# youtube = build('youtube', 'v3', developerKey=apiKey)
+youtube = build('youtube', 'v3', developerKey=apiKey)
+
+f = open('metadata.csv', 'a', encoding='utf-8', newline='')
+wr = csv.writer(f)
+
+MINIMUM_NUMBER = 300
+for key, value in categories.items():
+    print(f'Working on category {key}')
+    if value >= MINIMUM_NUMBER:
+        continue
+    
+    nextPageToken = None
+    while len(os.listdir('./thumbnails/images/' + key)) < MINIMUM_NUMBER:
+        response = youtube.search().list(
+            type='video',
+            videoCategoryId=key,
+            part='id',
+            maxResults=50,
+            pageToken=nextPageToken
+        ).execute()
+        videoIds = ','.join([item['id']['videoId'] for item in response['items']])
+        if videoIds == '':  # In case of no response
+            continue
+        nextPageToken = response['nextPageToken']
+
+        response = youtube.videos().list(
+            part='snippet',
+            id=videoIds
+            ).execute()
+
+        # Get category and thumbnail
+        for video in response['items']:
+            try:
+                videoId = video['id']
+                categoryId = video['snippet']['categoryId']
+
+                if 'maxres' in video['snippet']['thumbnails']:
+                    thumbnailUrl = video['snippet']['thumbnails']['maxres']['url']
+                elif 'standard' in video['snippet']['thumbnails']:
+                    thumbnailUrl = video['snippet']['thumbnails']['standard']['url']
+                elif 'high' in video['snippet']['thumbnails']:
+                    thumbnailUrl = video['snippet']['thumbnails']['high']['url']
+                elif 'medium' in video['snippet']['thumbnails']:
+                    thumbnailUrl = video['snippet']['thumbnails']['medium']['url']
+                else:
+                    thumbnailUrl = video['snippet']['thumbnails']['default']['url']
+                urllib.request.urlretrieve(thumbnailUrl, f'./thumbnails/images/{str(categoryId)}/{str(videoId)}.jpg')
+
+                wr.writerow([videoId, categoryId])
+            except Exception as e:
+                print(f'An error occured while getting video {videoId}\'s data.')
+                print(e)
+                continue
+        
+        if not nextPageToken:
+            break
+
+f.close()
